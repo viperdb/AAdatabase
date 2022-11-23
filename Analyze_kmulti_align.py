ff46011a8b2497205b49c72c02e35ebaed4e4b68
dante <dmontiel@scripps.edu>
2022-11-23 14:47:12 -0800

    Parametrized version of the analyze_kmulti_align

--- Analyze_kmulti_align.py
+++ Analyze_kmulti_align.py
@@ -1,8 +1,19 @@
-import json
+# ----------------------------------------------------------------
+# Created by    : Daniel Montiel
+# Created date   : 22/ Nov / 2022
+# -----------------------------------------------------------------
+"""
+    Entry point to get the Kalign and the annotated metadta
+    developed to provide epitope conservation
+"""
 
+import json
 from AnnotateAlignment_epitopes import AnnotateAlignmentEpitopes
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+import getopt
+
 
 class AnalyzeKmulti:
 
@@ -61,15 +72,52 @@ class AnalyzeKmulti:
         plt.bar( range(len(self.epitope_conservation)) , self.epitope_conservation, width=1)
         plt.show()
 
+def arguments_parsing():
+    global PDB_path
+    OPTIONS = {}
+    if len(sys.argv) > 1:
+        try:
+            opts, args = getopt.getopt(sys.argv[1:], "", ["kmulti=", "get_alignment", "get_annotation"])
+        except getopt.GetoptError as err:
+            print(f"Parameters error {err}")
+            exit(2)
+        OPTIONS["alignment"] = False
+        OPTIONS["annotation"] = False
+
+        for opt, arg in opts:
+            if opt == "--kmulti":
+                OPTIONS["kmulti"] = arg
+            elif opt == "--get_alignment":
+                OPTIONS["alignment"] = True
+                PDB_path = arg
+            elif opt == "--get_annotation":
+                OPTIONS["annotation"] = True
+
+        return OPTIONS
+    else:
+        print("Parameters missing")
+        print(" --kmulti [kpax k align structural alignment file]")
+        print(" --get_alignment -> Parameter to retrieve kalign file encoded in json format")
+        print(" --get_annotation -> Parameter to retrieve the structural conservation annotation")
+        exit(2)
+    print("Wait")
+
 
 if __name__ == "__main__":
     #path_file = "/home/dante/Julianne/antigens/test_cases/4FQI_B/4FQI_B.kmulti"
     path_file = "/home/dante/Julianne/antigens/kpax_results/5K9Q_B/5K9Q_B.kmulti"
+    params = arguments_parsing()
+    if "kmulti" in params:
+        path_file = params["kmulti"]
     obj = AnalyzeKmulti()
     obj.load_alignment(path_file)
     obj.calculate_conservation()
-    #print(str(obj.kmulti.kalign_data))
-    print(obj.to_json())
+    if params["alignment"]:
+        print(str(obj.kmulti.kalign_data))
+    else:
+        print(obj.to_json())
+
+    #print(obj.to_json())
     #print(str(test))
     #obj.plot_conservation()
     #print("ddd")
